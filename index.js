96395a3545aba1281cf68695155de228e4336473
wuchien <fe19ng@gmail.com>
2017-09-21 21:20:01 +0800

    test node version

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
+var WebSocketServer = require("ws").Server;
+var http = require("http");
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + "/"))
+app.use(express.static(__dirname + "/"));
 
-var server = http.createServer(app)
-server.listen(port)
+var server = http.createServer(app);
 
-console.log("http server listening on %d", port)
+app.get("/say", function(request, response) {
+  response.end("hihiok");
+});
 
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
+server.listen(port);
+
+console.log("http server listening on %d", port);
+
+var wss = new WebSocketServer({ server: server });
+console.log("websocket server created");
 
 wss.on("connection", function(ws) {
   var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+    ws.send(JSON.stringify(new Date()), function() {});
+  }, 1000);
 
-  console.log("websocket connection open")
+  console.log("websocket connection open");
 
   ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
+    console.log("websocket connection close");
+    clearInterval(id);
+  });
+});
